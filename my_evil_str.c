cbde5213a26db92c9a0ef28967e3f148d4aef9c5
Baron <arthur.baron@epitech.eu>
2019-10-03 22:40:04 +0200

    Task 05 my_evil_str

--- my_evil_str.c
+++ my_evil_str.c
@@ -10,14 +10,14 @@ int my_strlen(char const *str);
 char *my_evil_str(char *str)
 {
     int i = 0;
-    int size = my_strlen(str);
+    int length = my_strlen(str);
     int half = size / 2;
     char swap;
     
     for (i; i < half; i++) {
         swap = str[i];
-        str[i] = str[size - 1 - i];
-        str[size - 1 - i] = swap;
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = swap;
     }
     return str;
 }
