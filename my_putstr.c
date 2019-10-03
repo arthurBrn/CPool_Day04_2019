3c4559a0acbd9785d722587227867ca9ff35a650
Baron <arthur.baron@epitech.eu>
2019-10-03 17:03:01 +0200

    Task 02 my_putstr

---
+++ my_putstr.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** my_putstr
+** File description:
+** Task 02
+*/
+
+int my_putstr(char const *str)
+{
+    int a = 0;
+
+    while (str[a] != '\0') {
+        my_putchar(str[a]);
+        a++;
+    }
+    return (0);
+}
