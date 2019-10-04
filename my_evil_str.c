4eff4c8ffd3ed2e733dfa5f192da8127da8191ca
Baron <arthur.baron@epitech.eu>
2019-10-04 13:53:19 +0200

    Task 04 my_evil_str

--- my_evil_str.c
+++ my_evil_str.c
@@ -1,4 +1,4 @@
-²/*
+/*
 ** EPITECH PROJECT, 2019
 ** my_evil_str
 ** File description:
@@ -9,12 +9,11 @@ int my_strlen(char const *str);
 
 char *my_evil_str(char *str)
 {
-    int i = 0;
     int length = my_strlen(str);
     int half = length / 2;
     char swap;
     
-    for (i; i < half; i++) {
+    for (int i = 0; i < half; i++) {
         swap = str[i];
         str[i] = str[length - 1 - i];
         str[length - 1 - i] = swap;
